d875c9bfd411897b8be92d33d2affcf0c8d2f9e0
nobiliana <88566353+nobiliana@users.noreply.github.com>
2022-06-16 00:31:54 -0400

    Update cirque_kinetic.c
    Start code change from proof of concept to firmware ready.

--- cirque_kinetic.c
+++ cirque_kinetic.c
@@ -20,12 +20,7 @@ float yVal = 40; //will be int in the final op, error checking with float.
 //indicated in decimal value
 #define friction 0.35
 
-//test loop only
-int loopCount = 0;  
-int loopCycle = 3; //tester value, to simulate time check loops. implied time check loop
 int LIFTOFF = 0; //1 being contact, 0 being finger off. 
-int liftTest = 3; //phase 2 test, 2 lift test. lift 1 will reset same xVal and yVal, lift 2 will change angle. 
-//test loop end
 
 typedef struct {
     int xPoint;
@@ -58,44 +53,12 @@ void kineticVector (int xMouse, int yMouse){
 
 
 int main()
-{
-    printf("Hello World. \n");
-//    printf("test: %f\n", kineticVector(5, 6));
-
-    while(liftTest>0){ //test loop declaration. not necessary for final code. 
-        
-        kineticVector(xVal, yVal);
-    
-        printf("friction coefficient: %f\n", friction);
-        printf("vector magnitude: %f\n", mVector.magValue);
-        printf("vector angle (radians): %f\n", mVector.angValue);
-        printf("vector angle (degree): %f\n", mVector.angValue*rad2deg);
-        
-        printf("Begin kinetic loop \n");
-        printf("\n");
-        
-
+{   //change functions away from loops, as not necessary in QMK. 
+    while(liftTest>0){ //test loop declaration. not necessary for final code. will need replacement with a liftoff detection bool      
+        kineticVector(xVal, yVal); //will take deltaX and deltaY from drivers, and calculate into the xPoint and yPoints. 
         while(mVector.magValue > 0 && !LIFTOFF){
-            //printf("loop entered. logic to follow. \n");
-            if(loopCount >= loopCycle){ //test function variables, substitute for time variables.
-            
-                mVector.magValue = kineticDrag(mVector.angValue, mVector.magValue);
-                
-                printf("vector magnitude: %f\n", mVector.magValue);
-                printf("xVal: %f, yVal: %f\n", xVal, yVal);
-                printf("Vector Init: %f; Check: %f\n", mVector.angValue, atan2(yVal, xVal));
-                printf("\n");
-                loopCount = 0; //
-            } else { // test branch for pseudo time counter only. 
-                loopCount++;
-                printf("loop cycle: %d\n", loopCount);
-            }
-            
-            //reset test. 
-            if (liftTest > 1 && mVector.magValue < 3) {
-                LIFTOFF = 1;
-                printf("liftoff test. primary loop exit\n");
-            }
+            //printf("loop entered. logic to follow. \n");            
+                mVector.magValue = kineticDrag(mVector.angValue, mVector.magValue);            
         }
     }
     

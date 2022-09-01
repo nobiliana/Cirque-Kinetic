ac547fc94e329c2e525bda37d603fb69b1ae76c4
nobiliana <88566353+nobiliana@users.noreply.github.com>
2022-09-01 08:39:41 -0400

    Update cirque_kinetic.h
    moving variables into .h

--- cirque_kinetic.h
+++ cirque_kinetic.h
@@ -7,6 +7,8 @@ const float grav = 9.82;
 const float PI = 3.14159;
 const float rad2deg = 180/PI;
 
+float xVal = 30; //will be int in the final op, currently a test value. Will be equal to x delta.
+float yVal = 40; //will be int in the final op, currently a test value. Will be equal to y delta. 
 
 typedef struct {
     int xPoint;
